df2124db2317d0ddfe9ddfdd63015cb512af2540
paggida <danilopaggi@gmail.com>
2020-01-09 22:16:43 -0300

    Routes base structure

--- src/app/controllers/contractController.js
+++ src/app/controllers/contractController.js
@@ -2,7 +2,30 @@ const appConfig = require("../../config/app");
 const db = require("../functions/databaseFunctions");
 
 module.exports = {
-  async creationContract(req, res) {},
-  async uploadDocuments(req, res) {},
-  async ApprovalContract(req, res) {}
+  async listById(req, res) {
+    const { id } = req.params
+    return res.send(`route: listById ref: ${id}`)
+  },
+  async listByCpf(req, res) {
+    const { cpf } = req.params
+    return res.send(`route: listByCpf ref: ${cpf}`)
+  },
+  async creation(req, res) {
+    return res.send(`route: creation ref: ${JSON.stringify(req.body)}`)
+  },
+  async update(req, res) {
+    const { id } = req.params
+    return res.send(`route: update ref: ${id} / ${JSON.stringify(req.body)}`)
+  },
+  async uploadDocument(req, res) {
+    const { id } = req.params
+    const { type } = req.query
+    const { filename: doc } = req.file;
+    return res.send(`route: uploadDocument ref: ${id} / ${doc} / ${type}`)
+  },
+  async Approval(req, res) {
+    const { id } = req.params
+    const { approval } = req.body
+    return res.send(`route: Approval ref: ${id} / ${approval}`)
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const routes = express.Router();
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
+const ctrlContract = require("./app/controllers/contractController");
+
+routes.get("/contract/listById/:id", ctrlContract.listById);
+routes.get("/contract/listByCpf/:cpf", ctrlContract.listByCpf);
+routes.post("/contract/new", ctrlContract.creation);
+routes.put("/contract/update/:id", ctrlContract.update);
+routes.post("/contract/sendDocument/:id",upload.single("file"),ctrlContract.uploadDocument);
+routes.post("/contract/approval/:id", ctrlContract.Approval);
 
 module.exports = routes;
