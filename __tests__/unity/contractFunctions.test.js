d1b941a142c3727443b9422f145ca14676d4cd07
paggida <danilopaggi@gmail.com>
2020-01-12 10:57:24 -0300

    Contract functions feature

---
+++ __tests__/unity/contractFunctions.test.js
@@ -0,0 +1,61 @@
+const fnc = require('../../src/app/functions/contractFunctions');
+
+describe('RemoveInvalidFields function validation', () => {
+  it('should be able to remove all invalid value fields in an object', async () => {
+    const object = {
+      field01: 'OK',
+      field02: '',
+      field03: 0,
+      field04: false,
+    };
+    const responseObj = fnc.removeInvalidFields(object)
+    expect(Object.keys(responseObj)).toHaveLength(2);
+    expect(responseObj).toHaveProperty('field01','OK');
+    expect(responseObj).toHaveProperty('field04',false);
+  });
+});
+
+describe('AddStatusNewContract function validation', () => {
+  it('should be able to add the status that indicates a new contract', async () => {
+    const object = {
+      field01: 'OK',
+      field04: false,
+    };
+    const responseObj = fnc.addStatusNewContract(object)
+    expect(responseObj).toHaveProperty('status',0);
+  });
+});
+
+describe('isRequiredFieldsCorrect function validation', () => {
+  it('should be able to validate an object with all required fields and valid values', async () => {
+    const object = {
+      client_name: 'José',
+      client_email: 'jose@ig.com.br',
+      client_cpf: '99999999999',
+      value: 1000.00,
+      active: false
+    };
+    const response = fnc.isRequiredFieldsCorrect(object)
+    expect(response).toBeTruthy();
+  });
+  it('should no be able to validate an object without a required fields', async () => {
+    const object = {
+      client_name: 'José',
+      client_email: 'jose@ig.com.br',
+      client_cpf: '99999999999'
+    };
+    const response = fnc.isRequiredFieldsCorrect(object)
+    expect(response).toBeFalsy();
+  });
+  it('should not be able to validate an object with a invalid value', async () => {
+    const object = {
+      client_name: 'José',
+      client_email: 'jose@ig.com.br',
+      client_cpf: '',
+      value: 1000.00,
+    };
+    const response = fnc.isRequiredFieldsCorrect(object)
+    expect(response).toBeFalsy();
+  });
+});
+
---
+++ src/app/functions/contractFunctions.js
@@ -0,0 +1,32 @@
+const e = require("../Exceptions/apiExceptions");
+const errorTable = require("../Exceptions/contractExceptions");
+
+module.exports = {
+   removeInvalidFields(obj) {
+      for (let key in obj) {
+        if(!obj[key] && obj[key]!=false) delete obj[key];
+      }
+      return obj;
+  },
+  addStatusNewContract(obj={}) {
+    return ({...obj, status: 0});
+  },
+  isRequiredFieldsCorrect(obj={}){
+    return (_areRequiredFieldsExist(obj) && _areRequiredFieldsFilled(obj))
+  }
+};
+_areRequiredFieldsExist=(obj)=>{
+  const fieldsArray = Object.keys(obj)
+  if(fieldsArray.indexOf('client_name')<0 ||
+     fieldsArray.indexOf('client_email')<0 ||
+     fieldsArray.indexOf('client_cpf')<0 ||
+     fieldsArray.indexOf('value')<0) return false;
+  return true;
+}
+_areRequiredFieldsFilled=(obj)=>{
+  const valueFieldsArray = Object.values(obj)
+  for (let value in valueFieldsArray) {
+    if(!value && value!=false) return false;
+  }
+  return true;
+}
